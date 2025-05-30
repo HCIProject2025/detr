e2a8859a4c774b0e92aff70ccad756d240524fab
hong-seo <ghdtj07@gmail.com>
2025-05-30 21:09:55 +0900

    추가 커밋

---
+++ data/my_dataset/README.dataset.txt
@@ -0,0 +1,6 @@
+# Baseball Bat > 2024-10-28 11:28am
+https://universe.roboflow.com/yolov8main1/baseball-bat-no4hw
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
--- main.py
+++ main.py
@@ -55,6 +55,7 @@ def get_args_parser():
     parser.add_argument('--num_queries', default=100, type=int,
                         help="Number of query slots")
     parser.add_argument('--pre_norm', action='store_true')
+    parser.add_argument('--num_classes', default=3, type=int) 
 
     # * Segmentation
     parser.add_argument('--masks', action='store_true',
@@ -86,8 +87,7 @@ def get_args_parser():
 
     parser.add_argument('--output_dir', default='',
                         help='path where to save, empty for no saving')
-    parser.add_argument('--device', default='cuda',
-                        help='device to use for training / testing')
+    parser.add_argument('--device', default='cpu')
     parser.add_argument('--seed', default=42, type=int)
     parser.add_argument('--resume', default='', help='resume from checkpoint')
     parser.add_argument('--start_epoch', default=0, type=int, metavar='N',
